92b0c2658071b7b1e2d666bc8911bd159e81e762
BlueSky607 <dskang115@gmail.com>
2019-07-19 12:17:47 -0700

    If else statement with for loop and list

--- Practice/List.py
+++ Practice/List.py
@@ -47,3 +47,13 @@ u_input=int(input())
 print("당신이 원하는",u_input," 단은 다음과 같습니다")
 for i in range(1,10):
     print(u_input,"X",i,"=",u_input*i)
+
+
+math_data=[12,43,56,78,90,99,87,65,46,34,66,77,88,44,99,77,56]
+num_end=int(len(math_data))
+for i in range(0,num_end):
+    if (math_data[i]) >= 90: print("Grade A")
+    elif (math_data[i]) >= 80: print("Grade B")
+    elif (math_data[i]) >= 70: print("Grade C")
+    elif (math_data[i]) >= 60: print("Grade D")
+    else : print("Grade F")                
