d7db3e90e43125a03b569ac17980d709d3eec5c9
MrKangs <gykang00@gmail.com>
2019-07-26 23:45:00 -0700

    Tried to work on Excel

---
+++ Practice/excelDemo.py
@@ -0,0 +1,6 @@
+#Before you start the program, you need to go to cmd
+#and write 'pip install OpenPyXL'
+
+#엑셀과 파이썬 사용 전에 무조권 cmd에 들어가서 'pip install OpenPyXL' 라고 쳐야함!
+#Reference: https://openpyxl.readthedocs.io/en/stable/
+
