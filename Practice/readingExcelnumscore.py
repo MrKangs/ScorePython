c1623b76e0c1c901050a51327d60635cc333594d
BlueSky607 <dskang115@gmail.com>
2019-07-31 15:08:22 -0700

    need change in readingExcelnumscore.py file

--- Practice/readingExcelnumscore.py
+++ Practice/readingExcelnumscore.py
@@ -6,14 +6,20 @@ num = openpyxl.load_workbook('num.xlsx')
 sheet = num.active
 
 # Another way to print data
-
+counter = input("How many students' data do you have?")
 total = []
-for x in range(2, 34):  #replace 34 for counter
+for x in range(2, counter):  #replace 34 for counter
     rawdata = sheet['A' + str(x)].value
     print(rawdata)
     total.append(rawdata)
 
-print("The mean is ", stats.mean(total))
+
+num_mn = stats.mean(total)
+print("The mean is ", num_mn)
+
+sheet['A' + str(counter + 2)] = "Mean:"
+sheet['B' + str(counter + 2)] = num_mn
+num.save("num.xlsx")
 
 
 # It will print as a the same format in Excel 
