cbf6ee97ab914c333124d32418a4007a5fc3f65d
MrKangs <gykang00@gmail.com>
2019-07-31 11:49:00 -0700

    Add more content in readingExcel.py

--- Practice/readingExcel.py
+++ Practice/readingExcel.py
@@ -4,12 +4,23 @@ book = openpyxl.load_workbook('sample.xlsx')
 
 sheet = book.active
 
-cells = sheet['A1' : 'B6']
-#Reading from cells A1 to B6
+a1 = sheet['A1']
+a2 = sheet['A2']
+print(a1.value, a2.value)
+# printing the A1 and A2 data 
 
-for c1, c2 in cells:
-    print("{0:8} {1:8}".format(c1.value, c2.value))
+a3 = sheet.cell(row = 3, column = 1)
+print(a3.value)
+# Another way to print data
+
+cells = sheet['A4' : 'C9']
+#Reading from cells A4 to C9
+
+for c1, c2, c3 in cells:
+    print("{0:10} {1:7} {2:5}".format(c1.value, c2.value, c3.value))
 
 # It will print as a the same format in Excel 
-# (Caution!: if one of the cell is empty as the program reads it, it will return an error when it prints in the output)
+# {1:10} --> 1 = which row data, 10 = how many space from space 0 or between data
+# (Caution!: if one of the cell is empty as the program reads it, 
+# it will return an error when it prints in the output)
 
