251401213edbc3f63054d9801ebd621e0588f55d
MrKangs <gykang00@gmail.com>
2019-08-01 02:36:57 -0700

    add user input for controlling the chart

--- Project/readingExcelnumscore.py
+++ Project/readingExcelnumscore.py
@@ -12,18 +12,32 @@ num = openpyxl.load_workbook('num.xlsx')
 sheet = num.active
 
 counter = int(input("How many students' data do you have?: "))
-location = str(input("Which column the scores are located?: "))
-
+location_for_score = str(input("Which column the scores are located?: "))
+location_for_name = str(input("Which column the names are located?: "))
 total = []
 
-#def switch_for_chart(number):
-#    if(number == 'A'):
-#        return 1
+def switch_for_chart(number):
+    if(number == 'A'):
+        return 1
+    elif(number == 'B'):
+        return 2
+    elif(number == 'C'):
+        return 3
+    elif(number == 'D'):
+        return 4
+    elif(number == 'E'):
+        return 5
+    elif(number == 'F'):
+        return 6
+    elif(number == 'G'):
+        return 7
+    elif(number == 'H'):
+        return 8    
     
 
 
 for x in range(0, counter): 
-    rawdata = sheet[str(location) + str(x + 2)].value
+    rawdata = sheet[str(location_for_score) + str(x + 2)].value
     print(rawdata)
     total.append(rawdata)
 
@@ -58,11 +72,9 @@ sheet['B' + str(counter + 7)] = Variance
 
 chart = ScatterChart()
 
-xvalues = Reference(sheet, min_col = 3, min_row = 2, max_row = (counter + 1))
+xvalues = Reference(sheet, min_col = switch_for_chart(location_for_name), min_row = 2, max_row = (counter + 1))
 
-yvalues = Reference(sheet, min_col = 1, min_row = 2, max_row = (counter + 1))
-
-#switch_for_chart(location)
+yvalues = Reference(sheet, min_col = switch_for_chart(location_for_score), min_row = 2, max_row = (counter + 1))
 
 series = Series(values = yvalues, xvalues = xvalues, title = "2019 중간고사 성적")
 
